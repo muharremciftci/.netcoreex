b6d14c242ce9559eb1e5bd7b1bc2c03bbe6835f0
Muharrem Ciftci <muharremciftci34@gmail.com>
2022-07-17 17:54:35 +0300

    form validation and delete added

--- webui/Controllers/ProductController.cs
+++ webui/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using webui.Data;
 using webui.Models;
 using webui.ViewModels;
@@ -38,18 +39,18 @@ namespace webui.Controllers
 
             var products = ProductRepository.Products; //tüm listeleme
 
-           
-             if (id!=null) // id ye göre listeleme
+
+            if (id != null) // id ye göre listeleme
             {
-                products=products.Where(p=>p.CategoryId==id).ToList();
+                products = products.Where(p => p.CategoryId == id).ToList();
             }
 
             if (!string.IsNullOrEmpty(q)) //searchbox listeleme
             {
-                 products=products.Where(i=>i.Name.ToLower().Contains(q) || i.Description.ToLower().Contains(q)).ToList();
+                products = products.Where(i => i.Name.ToLower().Contains(q) || i.Description.ToLower().Contains(q)).ToList();
             }
 
-              var productViewModel = new ProductViewModel()
+            var productViewModel = new ProductViewModel()
             {
 
                 Products = products
@@ -62,15 +63,63 @@ namespace webui.Controllers
 
         public IActionResult Details(int id)
         {
-          
+
 
             return View(ProductRepository.GetProductById(id));
 
         }
-   
+
+
+        [HttpGet]
         public IActionResult Create()
         {
-            return View();
+
+            //Selected List kategorilerini getirme
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductRepository.AddProduct(p);
+                return RedirectToAction("List");
+
+            }
+
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
+
+            return View(p);
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
+
+            return View(ProductRepository.GetProductById(id));
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(Product p)
+        {
+            ProductRepository.EditProduct(p);
+            return RedirectToAction("List");
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int ProductId)
+        {
+            ProductRepository.DeleteProduct(ProductId);
+
+              return RedirectToAction("List");
+            
         }
     }
 }
--- webui/Models/Product.cs
+++ webui/Models/Product.cs
@@ -1,19 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace webui.Models
 {
     public class Product
     {
         public int ProductId {get;set;}
+
+        [Required(ErrorMessage="This field needs a value!")]      
+        
+        [StringLength(200,MinimumLength =10)]
+        
         public string Name {get;set;}
         
+        [Required(ErrorMessage ="Fiyat Girmelisiniz")]
+        [Range(1,50000)]
         public double Price {get;set;}
 
         public string Description {get;set;}
 
+        [Required]
         public string ImageUrl {get;set;}
 
         public bool IsApproved {get;set;}
 
-        public int CategoryId {get;set;}
+        [Required]
+        public int? CategoryId {get;set;}
         
 
 
